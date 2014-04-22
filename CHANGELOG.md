38f4de8debc8b1b1aa073f1a3a475becef74bd7f
tomchentw <developer@tomchentw.com>
2014-04-23 02:59:18 +0800

    chore(publish): v0.0.2 by Makefile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+<a name="0.0.2"></a>
+### 0.0.2  (2014-04-23)
+
+
+#### Features
+
+* **$image:** add angular-loading-bar(cfpLoadingBar) support ([e2b59a59](https://github.com/tomchentw/ng-preload-src/commit/e2b59a5933f26b7b5baa4277b3251e657b1f35c9))
+* **ng-preload-src:** API changed ([d295dfcf](https://github.com/tomchentw/ng-preload-src/commit/d295dfcf892b574591f169d21b9683f7303050b5))
+
+
 <a name="0.0.1"></a>
 ### 0.0.1  (2014-04-21)
 
--- ng-preload-src.js
+++ ng-preload-src.js
@@ -1,46 +1,62 @@
-/*! ng-preload-src - v 0.0.1 - Mon Apr 21 2014 20:03:38 GMT+0800 (CST)
+/*! ng-preload-src - v 0.0.2 - Wed Apr 23 2014 02:59:17 GMT+0800 (CST)
  * https://github.com/tomchentw/ng-preload-src
  * Copyright (c) 2014 [tomchentw](https://github.com/tomchentw);
  * Licensed [MIT](http://tomchentw.mit-license.org)
  */
 /*global angular:false*/
-(function(){
-  angular.module('ng-preload-src', []).service('ImgPreloader', ['$document', '$q'].concat(function($document, $q){
-    var element, $parent;
-    element = angular.element;
-    $parent = element('<div class="ng-hide"></div>');
-    $document.find('body').append($parent);
-    ImgPreloader.prototype.load = function(){
-      var url, defer, $img;
-      url = this.url, defer = this.defer;
-      $img = element("<img src='" + url + "'>");
-      $img.on('load', function(){
-        $img.remove();
-        defer.resolve(url);
-      });
-      $parent.append($img);
-      return defer.promise;
+(function(angular, bind, noop){
+  angular.module('ng-preload-src', []).factory('$image', ['$window', '$q', '$injector'].concat(function($window, $q, $injector){
+    var cfpLoadingBar;
+    cfpLoadingBar = $injector.get('cfpLoadingBar') || {
+      start: noop,
+      complete: noop
     };
-    function ImgPreloader(url){
-      this.url = url;
-      this.defer = $q.defer();
-    }
-    return ImgPreloader;
-  })).directive('preloadWithDefaultSrc', ['$interpolate', 'ImgPreloader'].concat(function($interpolate, ImgPreloader){
-    var bind;
-    bind = angular.bind;
-    function changeFn(setter, url){
-      new ImgPreloader(url).load().then(setter);
-    }
-    function postLinkFn($scope, $element, $attrs){
-      $scope.$watch($interpolate($attrs.preloadSrc), bind(void 8, changeFn, bind($attrs, $attrs.$set, 'ngSrc')));
+    function onLoadEvent(url){
+      cfpLoadingBar.complete();
+      this.resolve(url);
     }
     return {
+      preload: function(url){
+        var defer, image;
+        defer = $q.defer();
+        image = new $window.Image;
+        image.src = url;
+        cfpLoadingBar.start(0);
+        angular.element(image).on('load', bind(defer, onLoadEvent, url));
+        return defer.promise;
+      }
+    };
+  })).directive('preloadWithDefaultSrc', ['$interpolate', '$image'].concat(function($interpolate, $image){
+    var PreloadCtrl;
+    PreloadCtrl = (function(){
+      PreloadCtrl.displayName = 'PreloadCtrl';
+      var prototype = PreloadCtrl.prototype, constructor = PreloadCtrl;
+      prototype.onSrcChanged = function(newUrl){
+        this.$attrs.$set('ngSrc', this.$attrs.preloadWithDefaultSrc);
+        this.lastUrl = newUrl;
+        $image.preload(newUrl).then(this.onImageLoaded);
+      };
+      prototype.onImageLoaded = function(newUrl){
+        if (this.lastUrl === newUrl) {
+          this.$attrs.$set('ngSrc', newUrl);
+        }
+      };
+      PreloadCtrl.$inject = ['$scope', '$attrs'];
+      function PreloadCtrl($scope, $attrs){
+        this.$scope = $scope;
+        this.$attrs = $attrs;
+        this.lastUrl = void 8;
+        this.onImageLoaded = bind(this, this.onImageLoaded);
+        $scope.$watch($interpolate($attrs.preloadSrc), bind(this, this.onSrcChanged));
+      }
+      return PreloadCtrl;
+    }());
+    return {
+      controller: PreloadCtrl,
       compile: function(tElement, tAttrs){
         tAttrs.$set('preloadSrc', tAttrs.ngSrc);
         tAttrs.$set('ngSrc', tAttrs.preloadWithDefaultSrc);
-        return postLinkFn;
       }
     };
   }));
-}).call(this);
+}.call(this, angular, angular.bind, angular.noop));
